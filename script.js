c359c39e54bdc509cc6a9205925bd9cd89c03088
Lynn Long Jr <lynnlong000@gmail.com>
2017-07-25 23:40:59 +0000

    update the script.js to print ot console when source is seleceted

--- script.js
+++ script.js
@@ -10,15 +10,36 @@ $(document).ready(function() {
             // console.log(response.sources[2]);
             var sources = response.sources;
             // console.log(sources[2]);
-            var html = "<select class='form-control' id='source'>";
+            var html = "<select class='form-control' id='sourceOption'>";
             $.each(sources, function(index, source){
                 // console.log(source);
-                html += "<option>" + source.name + "</option>"
-            })
+                html += "<option value = '"+source.id+"'>" + source.name + "</option>";
+            });
             html += "</select>";
-            console.log(html)
-            $(".form-group").html(html)
-            
+           // console.log(html);
+            $(".form-group").html(html);
+
         }
-    })
+    });
+    
+    $("#source").submit(function(event){
+        event.preventDefault();
+        var id = $('#sourceOption').val();
+        var url = 'https://newsapi.org/v1/sources';
+        var data = {apikey:"b9c5d0d41c8b45e592490b7114c532ce",source:id};
+        $.ajax({
+            url:url,
+            data:data,
+            type:"GET",
+            success:function(response){
+                var articles = response.articles ;
+                console.log(response);
+                
+                
+            }
+        });
+
+        
+    });
+    
 })
